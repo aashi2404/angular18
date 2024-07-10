9fc936a11d06d5ae9ac6bd3b48f6b81a50b6cb91
aashi2404 <2002aashu@gmail.com>
2024-07-10 17:02:08 +0530

    validated form

--- my-form/src/app/app.component.ts
+++ my-form/src/app/app.component.ts
@@ -1,29 +1,35 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component,OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators   } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-form';
   myid: any;
-  myform: any;
+  myform!: FormGroup;
+
   ngOnInit() {
     this.myform= new FormGroup({
-      username : new FormControl(''),
-      password : new FormControl('')
+      username : new FormControl('', [Validators.required, Validators.minLength(4)]),
+      gmail : new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@gmail\.com$/)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(5)])
     });
+    this.display();
   }
 
   display() {
-    this.myid = localStorage.getItem("formdata");
+    const formData = localStorage.getItem('formdata');
+    this.myid = formData ? JSON.parse(formData) : '';
   }
 
   onSubmit(){
-    localStorage.setItem("formdata",JSON.stringify(this.myform.value));
-    this.display();
+    if (this.myform.valid) {
+      localStorage.setItem('formdata', JSON.stringify(this.myform.value));
+      this.display();
+    }
   }
 
   onReset() {
